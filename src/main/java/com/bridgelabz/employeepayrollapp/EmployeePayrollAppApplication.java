bf8fff707dcbf5d3ceca8fcb1583fb8bc7c9d304
unknown <shaikh.shahazad@bridgelabz.com>
2024-01-31 16:41:22 +0530

    Added: Username and password create for the sql user and env setup

--- src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppApplication.java
@@ -14,7 +14,8 @@ public class EmployeePayrollAppApplication {
 		System.out.println("Welcome to employee Payroll app");
 		ApplicationContext context =SpringApplication.run(EmployeePayrollAppApplication.class, args);
 		log.info("Employee Payroll App Started in {} Environment",context.getEnvironment().getProperty("environment"));
-
+		log.info("Employee Payroll DB User is {}",
+				context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
